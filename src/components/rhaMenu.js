1bbe26d0ac905e944b640ea48b70742f2e087032
chanales <floreschanales282@gmail.com>
2020-01-27 17:21:54 -0500

    add RHA Logo and change settings of nav

--- src/components/rhaMenu.js
+++ src/components/rhaMenu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Logo from "../images/RHA_Logo_Vector.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
@@ -9,7 +10,7 @@ import Home from "./home";
 import Eboard from "./eboard";
 import Committees from "./committees";
 import HallCouncils from "./hallCouncils";
-import Docs from './doc';
+import Docs from "./doc";
 
 function Menu() {
   return (
@@ -17,35 +18,48 @@ function Menu() {
       <div>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Navbar.Brand as={Link} to="/home">
+            <img
+              alt=""
+              src={Logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
             Residence Hall Association
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto"></Nav>
             <Nav>
-              <NavDropdown title="Foundations" id="collasible-nav-dropdown">
+              <NavDropdown title="About" id="collasible-nav-dropdown">
                 <NavDropdown.Item as={Link} to="/about">
                   About Us
                 </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/eboard">
+                  Meet the Exective Board
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to="/docs">
+                  Documents and Forms
+                </NavDropdown.Item>
+                <NavDropdown.Item href="https://njit.campuslabs.com/engage/organization/rha">
+                  Regional Business Conference
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Get Involved" id="collasible-nav-dropdown">
                 <NavDropdown.Item as={Link} to="/committees">
                   Committees
                 </NavDropdown.Item>
                 <NavDropdown.Item as={Link} to="/hallCouncils">
                   Hall Councils
                 </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item as={Link} to="/docs">
-                  Documents/Forms
-                </NavDropdown.Item>
-                <NavDropdown.Item href="https://njit.campuslabs.com/engage/organization/rha">
-                  Regional Business Conference
-                </NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link as={Link} to="/eboard">E-Board</Nav.Link>
               <Nav.Link as={Link} to="/home">
-                Spoons
+                BOTH
               </Nav.Link>
-              <Nav.Link href="https://njit.campuslabs.com/engage/organization/rha">Events</Nav.Link>
+              <Nav.Link href="https://njit.campuslabs.com/engage/organization/rha">
+                Events
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
