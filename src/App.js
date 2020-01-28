0dfb76ed87bb643926c8ad224ea2e6a62c7d9f50
chanales <floreschanales282@gmail.com>
2020-01-27 21:23:21 -0500

    remove bounce animation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
-import { bounce } from "react-animations";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Menu from "./components/rhaMenu";
-import Home from "./components/home";
 import { BrowserRouter } from 'react-router-dom';
 import "./App.css";
 
 function App() {
-  const Bounce = styled.div`
-    animation: 2s ${keyframes`${bounce}`} infinite;
-  `;
   return (
     <div className="App">
       <BrowserRouter>
