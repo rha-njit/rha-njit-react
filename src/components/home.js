3128a2ea37bd1437bd196acd23a2265c6efecba2
chanales <floreschanales282@gmail.com>
2020-01-27 21:24:09 -0500

    add animations to page

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,22 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+import { fadeIn } from 'react-animations';
+import Card from "react-bootstrap/Card";
 import "./home.css";
 
-function Home (){
-
-    return (
-        <div>
-            <div className="bg" />
-        </div>
-    );
+function Home() {
+  const FadeIn = styled.div`
+    animation: 6s ${keyframes`${fadeIn}`} infinite;
+  `;
+  return (
+    <div>
+      <Card className="bg-dark text-white">
+        <Card.Img className="bg"/>
+        <Card.ImgOverlay className="text-center">
+          <FadeIn><Card.Title as="h1" className="center">Residence Hall Association</Card.Title></FadeIn>
+        </Card.ImgOverlay>
+      </Card>
+    </div>
+  );
 }
-export default Home; 
+export default Home;
