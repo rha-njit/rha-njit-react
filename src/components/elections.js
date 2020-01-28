37d4c52ce2403cf80dcd24383b4fd77865f58185
chanales <floreschanales282@gmail.com>
2020-01-27 21:26:14 -0500

    create elections page

---
+++ src/components/elections.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import "../App.css";
+
+function Elections() {
+  return (
+    <div>
+      <Jumbotron>
+        <h1>RHA Elections</h1>
+        <br />
+        <h2>Hello</h2>
+      </Jumbotron>
+    </div>
+  );
+}
+export default Elections;
