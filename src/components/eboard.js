00b6ebf08af05c262746f3048018f85f3222d91e
chanales <floreschanales282@gmail.com>
2020-01-28 20:03:07 -0500

    add eboard photos most of them

--- src/components/eboard.js
+++ src/components/eboard.js
@@ -4,9 +4,20 @@ import CardDeck from "react-bootstrap/CardDeck";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Mark from "../images/eboard/mark.jpg";
+import Matt from "../images/eboard/matt.jpg";
+import Sam from '../images/eboard/sam.jpg';
+import Portia from "../images/eboard/portia.jpg";
+import Reesha from "../images/eboard/reesha.jpg";
+import Shruti from "../images/eboard/shruti.jpg";
+import styled, { keyframes } from "styled-components";
+import { flipInY } from 'react-animations';
 import "../App.css";
 
 function Eboad() {
+  const FlipInY = styled.div`
+    animation: 4s ${keyframes`${flipInY}`};
+  `;
   return (
     <div>
       <Container>
@@ -43,9 +54,9 @@ function Eboad() {
               <Card.Img src="holder.js/100px180" />
             </Card>
           </Col>
-          <Col xs={8}>
+          <Col xs={5}>
             <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
+              <FlipInY><Card.Img variant="top" src={Sam} /></FlipInY>
               <Card.Body>
                 <Card.Title>President</Card.Title>
                 <Card.Text>
@@ -70,7 +81,7 @@ function Eboad() {
       <br />
       <CardDeck>
         <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Img variant="top" src="holder.js/100px180" />
           <Card.Body>
             <Card.Title>Vice President of Administration</Card.Title>
             <Card.Text>
@@ -84,7 +95,7 @@ function Eboad() {
           </Card.Footer>
         </Card>
         <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Img variant="top" src={Reesha} />
           <Card.Body>
             <Card.Title>Vice President of Programming</Card.Title>
             <Card.Text>
@@ -97,7 +108,7 @@ function Eboad() {
           </Card.Footer>
         </Card>
         <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Img variant="top" src={Portia} />
           <Card.Body>
             <Card.Title>National Communications Coordinator</Card.Title>
             <Card.Text>
@@ -115,7 +126,7 @@ function Eboad() {
       <br />
       <CardDeck>
         <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Img variant="top" src={Shruti}/>
           <Card.Body>
             <Card.Title>National Communications Coordinator in training</Card.Title>
             <Card.Text>
@@ -129,7 +140,7 @@ function Eboad() {
           </Card.Footer>
         </Card>
         <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Img variant="top" src={Matt} />
           <Card.Body>
             <Card.Title>Treasurer</Card.Title>
             <Card.Text>
@@ -187,7 +198,7 @@ function Eboad() {
           </Card.Footer>
         </Card>
         <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Img variant="top" src={Mark} />
           <Card.Body>
             <Card.Title>Membership Officer</Card.Title>
             <Card.Text>
