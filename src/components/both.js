7e83e95a7d4f0fbc3797aaaa1268734410878d2f
chanales <floreschanales282@gmail.com>
2020-01-28 20:03:21 -0500

    start of both page

--- src/components/both.js
+++ src/components/both.js
@@ -1,7 +1,4 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
 import Jumbotron from "react-bootstrap/Jumbotron";
 import "../App.css";
 
@@ -12,32 +9,8 @@ function Both() {
         <h1>Battle Of The Halls</h1>
         <br />
         <h2>Join a Committee Today!</h2>
-        <p>Committees are great way for general body memebers to get involved within RHA.</p>
       </Jumbotron>
       
-      <CardDeck>
-        <Card>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Body>
-            <Card.Title>Grilled Cheese Service</Card.Title>
-            <Card.Text>
-              1/2 Sandwich - $1
-              <br/>
-              Whole Sandwich - $2
-              <br/>
-              CapriSun - $1
-            </Card.Text>
-            <Button variant="primary">RHA Grilled Cheese</Button>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Thursday Nights: 10pm to 1am </small>
-          </Card.Footer>
-        </Card>
-        <Card>
-        </Card>
-      </CardDeck>
     </div>
   );
 }
