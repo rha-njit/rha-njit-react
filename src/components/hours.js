3e6fdef05bee46ae68872ea1043861038f980924
chanales <floreschanales282@gmail.com>
2020-02-10 18:31:39 -0500

    create office hours page

---
+++ src/components/hours.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Background from "../images/backGroundBlack.png";
+import "../App.css";
+import "./home.css";
+
+function Hours() {
+  return (
+      
+    <div
+      style={{
+        backgroundImage: `url(${Background})`,
+        overflowX: "hidden"
+      }}
+    >
+      <Jumbotron className="backg">
+        <h1 className="textColor">Office Hours</h1>
+      </Jumbotron>
+    </div>
+  );
+}
+export default Hours;
