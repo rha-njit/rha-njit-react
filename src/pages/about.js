153d7977ef5d44eed27ea0c0d2dd3c49d3cc9564
chanales <floreschanales282@gmail.com>
2020-04-26 23:15:31 -0400

    create about to merge aboutUs and home files

---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./aboutStyle.css";
+
+function About() {
+  return (
+    <div>
+      <header>
+        <h1>Residence Hall Association</h1>
+      </header>
+      <main>
+          <h3>Sailor Ipsum</h3>
+          <p>
+            Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast
+            yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin
+            grog yardarm hempen halter furl. Swab barque interloper chantey
+            doubloon starboard grog black jack gangway rutters.
+          </p>
+        </main>
+    </div>
+  );
+}
+export default About;
