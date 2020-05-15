601eeccb7bfc80c950d2381ae0dfbf39f3201ed0
chanales <floreschanales282@gmail.com>
2020-05-15 16:55:02 -0400

    remove boostrap style

--- src/pages/both.js
+++ src/pages/both.js
@@ -1,16 +1,10 @@
 import React from "react";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import "../App.css";
 
 function Both() {
   return (
     <div>
-      <Jumbotron>
-        <h1>Battle Of The Halls</h1>
-        <br />
-        <h2>Coming Soon</h2>
-      </Jumbotron>
-      
+      <h1>Battle of Halls</h1>
+      <p>Spoons link should go here</p>
     </div>
   );
 }
--- src/pages/elections.js
+++ src/pages/elections.js
@@ -1,15 +1,9 @@
 import React from "react";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import "../App.css";
 
 function Elections() {
   return (
     <div>
-      <Jumbotron>
-        <h1>RHA Elections</h1>
-        <br />
-        <h2>Coming Soon</h2>
-      </Jumbotron>
+      
     </div>
   );
 }
