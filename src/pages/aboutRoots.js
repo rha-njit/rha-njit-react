0fa9c67f27ade5b1e71b3f1772ce374e68785ba1
chanales <floreschanales282@gmail.com>
2020-05-15 20:26:13 -0400

    init aboutRoot page & del old aboutUs page

---
+++ src/pages/aboutRoots.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import Sidehoe from "../images/sideHoe.png";
+import Leadership from "../images/NJIT+RUN.jpg";
+import FrankieCutie from "../images/frankieCutie2.jpg";
+import Background from "../images/backGroundBlack.png";
+import ProgrammingPhoto from "../images/RHA_Homecoming.jpg";
+
+function Roots() {
+  return (
+    <div>
+      <h1>Our Roots</h1>
+      <h2>Why should I get involved? What things does RHA focus on?</h2>
+      {/*Advocacy
+        Through resident feedback, we recognize a problem and create a
+              solution. We are here to better your residential experience
+              whether it be free laundry or simply maintaining your residence
+              hall.
+        Programming
+        Our innovative programs (that include, but are not limited to:
+              cultural awareness and health/wellness) are also an opportunity
+              for residents to interact with one another and engage everyone in
+              the NJIT community
+        Leadership Development
+        As a student run organization, we strive to strengthen our
+              residents abilities to lead through involvement on campus
+        Residential Hall Council
+        Every residence hall belongs to a Residential Hall Council.
+                    Similar to a city hall meeting, this is the easiest way for
+                    residents to directly get involved in their community.
+        General Body Meetings
+        Every week on Firday the Residence Hall Association holds a
+                    General Body Meeting. This would be a great place to voice
+                    your concerns about living on campus.
+        The Committees
+        The Programming and Delegation committees are a fun way to
+                    get involved with the organization. Visit the page for more
+                    information.
+      */}
+      
+    </div>
+  );
+}
+export default Roots;
